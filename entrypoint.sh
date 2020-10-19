96a865f79315bac79292b78a998e2f752682d723
Tom Wieland <tom.wieland@gmail.com>
2020-10-19 21:32:49 +0200

    feat: added an option for specifying the dockerfile path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,8 @@
-#!/bin/sh -l
+#!/usr/bin/env bash
 
 IMAGE_NAME="$INPUT_GCR_HOST/$GOOGLE_PROJECT_ID/$INPUT_IMAGE_NAME:$INPUT_IMAGE_TAG"
+DOCKERFILE_PATH="${INPUT_DOCKERFILE:-.}"
 
 echo $GCLOUD_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
-docker build -t $IMAGE_NAME .
+docker build -t $IMAGE_NAME $DOCKERFILE_PATH
 docker push $IMAGE_NAME
