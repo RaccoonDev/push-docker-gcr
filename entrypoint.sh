2b96f7943ab3b71390f0ab9ae8aa2d197dc326ae
Tom Wieland <tom.wieland@gmail.com>
2020-10-19 22:58:46 +0200

    changed bash back to sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh -l
 
 IMAGE_NAME="$INPUT_GCR_HOST/$INPUT_GCR_PROJECT_ID/$INPUT_DOCKER_IMAGE_NAME:$INPUT_DOCKER_IMAGE_TAG"
-CONTEXT_PATH="${INPUT_CONTEXT:-.}"
 
 echo $INPUT_GCR_SERVICE_KEY | docker login -u _json_key --password-stdin https://$INPUT_GCR_HOST/
 docker build -t $IMAGE_NAME $INPUT_DOCKER_CONTEXT
